e924b78f93b71e2dfae0fda39e76b74e54a01d24
murmelgrumpf <murmelgrumpf@aol.com>
2021-05-30 18:48:22 +0200

    Optimized code

--- Categorymanager.i18n.magic.php
+++ Categorymanager.i18n.magic.php
@@ -1,12 +1,12 @@
 <?php
- 
+
 $magicWords = [];
- 
+
 $magicWords['en'] = [
     // Case insensitive.
     'mag_catformat' => [ 0, 'catformat' ],
 ];
- 
+
 $magicWords['de'] = [
     'mag_catformat' => [ 0, 'catformat' ],
-];
+]; 
