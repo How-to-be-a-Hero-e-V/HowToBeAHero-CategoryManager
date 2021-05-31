f83b300059279031ec1d1233a05fbf187312964b
murmelgrumpf <murmelgrumpf@aol.com>
2021-05-31 02:03:47 +0200

    removed debugger

--- Categorymanager.php
+++ Categorymanager.php
@@ -3,14 +3,12 @@
 class Categorymanager {
     public static function onParserFirstCallInit( $parser ) {
         $parser->setFunctionHook( 'mag_catformat', [ self::class, 'categoryFormat' ] );
-		MWDebug::init();
-		MWDebug::log('Blaaaa');
+
         return true;
     }
 
     public static function categoryFormat( $parser) {
-		MWDebug::init();
-		MWDebug::log('Bliii');
+
 		return ["
 		<style>
 		div#mw-subcategories h2, div#mw-subcategories p, div#mw-pages h2, div#mw-pages p{
